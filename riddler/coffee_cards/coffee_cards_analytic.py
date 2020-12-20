0f96fc0f4d53f9ece53fb7af02c037067f710fac
Richard Cronce <richard.cronce@outlook.com>
2020-12-19 19:18:29 -0600

    Fixed error in calculating mean result

--- riddler/coffee_cards/coffee_cards_analytic.py
+++ riddler/coffee_cards/coffee_cards_analytic.py
@@ -7,12 +7,13 @@ def bcoeff(N,K):
 l=list(range(11))+[20,30,40,50]
 mean_result=0
 mlast=0
-for m in l:
+for m in range(51):
     mean_result = mean_result + m*bcoeff(100-m,50)*(0.5**(100-m))
-    if m>mlast+1:
-        print("...")
-    print(m,bcoeff(100-m,50)*(0.5**(100-m)))
-    mlast=m
+    if m in l:
+        if m>mlast+1:
+            print("...")
+        print(m,bcoeff(100-m,50)*(0.5**(100-m)))
+        mlast=m
 
 print()
 print("Mean remaining drinks = ",mean_result)
